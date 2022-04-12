d105f7927a25b69d886c5bdf63b68cbd4fe77c1b
Lightsilverfire <bliksem@wmmt.nl>
2022-04-12 09:09:54 +0200

    comments toevoegen

--- poc1.js
+++ poc1.js
@@ -1,9 +1,11 @@
+//sprite bepalen en laden
 let img = new Image();
 img.src = 'Green-Cap-Character-16x18.png';
 img.onload = function() {
     init();
 };
  
+//sprite op canvas zetten met goede frame
 let canvas = document.querySelector('canvas');
 let ctx = canvas.getContext('2d');
 let touch;
@@ -24,12 +26,12 @@ const cycleLoop = [0, 1, 0, 2];
 let currentLoopIndex = 0;
 let frameCount = 0;
  
- 
+ //touch event, touchstart is je vinger op het scherm is, touchend is als je vinger niet meer op het scherm is
 document.getElementById("canvas").addEventListener("touchstart", function (){touch = true});
 document.getElementById("canvas").addEventListener("touchend", function (){touch = false});
 
 
- 
+ //sprite laten lopen als het scherm aangeraakt wordt
 function step() {
 
   if (touch){
@@ -46,6 +48,7 @@ function step() {
         currentLoopIndex = 0;
     }
   }
+  //als er niet meer op het scherm gedrukt wordt, dan maakt hij het scherm leeg en zet de eerste frame op het canvas
   else
   {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
